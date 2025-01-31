4e9a7de76648ad85b22dd8b60800890f0d7c9936
DeepZima <deepzima@outlook.com>
2025-01-31 18:02:02 +0100

    feat: propagation e2e testing, manage first kind confs

---
+++ envoy/sidecar-envoy-examples/e2e/serviceB/src/main.rs
@@ -0,0 +1,18 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello from Service B!" }));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 5002));
+    println!("Service B running on {}", addr);
+
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    // axum::serve::bind(&addr)
+    //     .serve(app.into_make_service())
+    //     .await
+    //     .unwrap();
+}
